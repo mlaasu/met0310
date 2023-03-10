5387a8e002849cebd5be45813f1ff8c453793042
mlaasu <mark.laasu@outlook.com>
2023-03-10 10:41:30 +0200

    fixed ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mark/met0310/src
-# Build directory: /home/mark/met0310/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
