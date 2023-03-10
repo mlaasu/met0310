62b9ccf87c5ed05672df6849e73b23aaac80cd6a
mlaasu <mark.laasu@outlook.com>
2023-03-10 10:15:01 +0200

    9.03.23 practice 4: write publisher, subscriber, service, client, bag

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mark/met0310/src
+# Build directory: /home/mark/met0310/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mark/met0310/src/beginner_tutorials
+# Build directory: /home/mark/met0310/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
